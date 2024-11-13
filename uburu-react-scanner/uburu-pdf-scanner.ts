c0975f03e7589995e21c904ba3a11fc8204ac3db
Williams Alex <godswillwilliams9@gmail.com>
2024-11-13 10:34:59 +0100

    Feat: Added file size calculator

--- uburu-react-scanner/uburu-pdf-scanner.ts
+++ uburu-react-scanner/uburu-pdf-scanner.ts
@@ -1,6 +1,6 @@
 // Install the required packages
 
-import { useRef, useState, useEffect, useCallback } from "react";
+import { useRef, useState, useEffect, useMemo, useCallback } from "react";
 import jsPDF from "jspdf";
 import axios from "axios";
 
@@ -21,14 +21,16 @@ export function useUburuPdfScanner({ fileName }: { fileName?: string }) {
     setLoadingCameraView(true);
     try {
       const mediaStream = await navigator.mediaDevices.getUserMedia({
-        video: { facingMode: { ideal: "environment" } }
+        video: { facingMode: { ideal: "environment" } },
       });
       setLoadingCameraView(false);
       setStream(mediaStream);
       if (uburuVideoRef.current) uburuVideoRef.current.srcObject = mediaStream;
     } catch (error) {
       console.error("Error accessing the camera:", error);
-      console.log("Could not access the camera. Please check your permissions.");
+      console.log(
+        "Could not access the camera. Please check your permissions."
+      );
       setLoadingCameraView(false);
     }
   }, []);
@@ -49,7 +51,13 @@ export function useUburuPdfScanner({ fileName }: { fileName?: string }) {
       uburuCanvas.width = uburuVideo.videoWidth;
       uburuCanvas.height = uburuVideo.videoHeight;
       const context = uburuCanvas.getContext("2d");
-      context?.drawImage(uburuVideo, 0, 0, uburuCanvas.width, uburuCanvas.height);
+      context?.drawImage(
+        uburuVideo,
+        0,
+        0,
+        uburuCanvas.width,
+        uburuCanvas.height
+      );
 
       const imgData = uburuCanvas.toDataURL("image/jpeg", 0.7);
       const imgId = Date.now();
@@ -63,7 +71,9 @@ export function useUburuPdfScanner({ fileName }: { fileName?: string }) {
   }, []);
 
   const deleteUburuImage = useCallback((imgId: number) => {
-    setCapturedImages((prevImages) => prevImages.filter((img) => img.id !== imgId));
+    setCapturedImages((prevImages) =>
+      prevImages.filter((img) => img.id !== imgId)
+    );
   }, []);
 
   const downloadUburuScanPDF = useCallback(() => {
@@ -133,6 +143,37 @@ export function useUburuPdfScanner({ fileName }: { fileName?: string }) {
     setCapturedImages([]);
   }, []);
 
+  const determineFileWeightType = useCallback((size) => {
+    switch (true) {
+      case size < 1024:
+        return `${size} byte${size === 1 ? "" : "s"}`;
+      case size < 1024 * 1024:
+        return `${(size / 1024).toFixed(2)} KB`;
+      case size < 1024 * 1024 * 1024:
+        return `${(size / (1024 * 1024)).toFixed(2)} MB`;
+      default:
+        return `${(size / (1024 * 1024 * 1024)).toFixed(2)} GB`;
+    }
+  }, []);
+
+  const totalFileSize = useMemo(() => {
+    const imageFiles = capturedImages.map((i) => i.data);
+
+    const totalSize = imageFiles.reduce((acc, imageFile) => {
+      const byteString = atob(imageFile.split(",")[1]);
+      const byteArray = new Uint8Array(byteString.length);
+
+      for (let i = 0; i < byteString.length; i++) {
+        byteArray[i] = byteString.charCodeAt(i);
+      }
+
+      const blob = new Blob([byteArray], { type: "image/jpeg" });
+      return acc + blob.size;
+    }, 0);
+
+    return determineFileWeightType(totalSize);
+  }, [capturedImages, determineFileWeightType]);
+
   useEffect(() => {
     return () => {
       if (stream) {
@@ -147,6 +188,7 @@ export function useUburuPdfScanner({ fileName }: { fileName?: string }) {
     capturedImages,
     loadingCameraView,
     uploadingPDF,
+    totalFileSize,
     deleteAllUburuCapturedImages,
     turnOnUburuScanCamera,
     turnOffUburuScanCamera,
